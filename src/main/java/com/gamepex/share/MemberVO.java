e96858c8ff04670c4694651d7b7c1be6fd0c58f4
Hanbyeol-Kwon <valuable9951@gmail.com>
2025-12-19 22:11:50 +0900

    Update MemberVO.java

--- src/main/java/com/gamepex/share/MemberVO.java
+++ src/main/java/com/gamepex/share/MemberVO.java
@@ -16,6 +16,7 @@ public class MemberVO {
 	private String mb_phone;
 	private String mb_zipcode;
 	private String mb_addr;
+	private String mb_addrdet;
 	private int mb_grade;
 	private int mb_state;
 	private Date mb_regdate;
