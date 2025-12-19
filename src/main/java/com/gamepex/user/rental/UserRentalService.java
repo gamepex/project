7df60d70e5dd4d232394ff7dd5777332375c0728
HanbyeolKwon <valuable9951@gmail.com>
2025-12-20 00:43:00 +0900

    1219업데이트

--- src/main/java/com/gamepex/user/rental/UserRentalService.java
+++ src/main/java/com/gamepex/user/rental/UserRentalService.java
@@ -10,7 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.gamepex.admin.console.AdminConsoleDAO;
 import com.gamepex.admin.gametitle.AdminGameTitleDAO;
-import com.gamepex.share.RentDetailVO;
 import com.gamepex.share.RentalDTO;
 import com.gamepex.share.RentalVO;
 
@@ -24,27 +23,9 @@ public class UserRentalService {
 	@Inject
 	private AdminGameTitleDAO adminGameTitleDAO;
 
-	
-	@Transactional
-	public void register(RentalVO rentalInfo, List<RentDetailVO> detailList) throws Exception {
-		userRentalDAO.register(rentalInfo);
-		int price = 0;
-		
-		for(RentDetailVO detail : detailList) {
-			detail.setRt_no(rentalInfo.getRt_no());
-			
-			if(detail.getRd_type().matches("playstation5|xbox|virtual|steamdeck|nintendo")) {
-				price = adminConsoleDAO.getConsoleOne(detail.getRd_serial()).getCon_price();
-			} //else {
-				//price = adminGameTitleDAO.getGameTitleOne(detail.getRd_serial()).getTtl_price();
-			//}
-			detail.setRd_price(price);
-		}
-		userRentalDAO.registerDetail(detailList);
-		
+	public void register(RentalVO rentalVO) throws Exception {
+		userRentalDAO.register(rentalVO);
 	}
-
 	
-	
-	
+		
 }
